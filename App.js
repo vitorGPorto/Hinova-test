872ea74e9c4a337229bc5318f75e5bb4436bd417
VitorGPoro <vitorgsporto01@gmail.com>
2022-11-29 10:05:05 -0300

    projeto de teste de conhecimento

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { StatusBar } from 'expo-status-bar';
 
-import ToBarRoute from "./src/routes/toBar.routes";
+
 import StackRouter from "./src/routes/stack.routes";
  import {NavigationContainer} from "@react-navigation/native"
 
